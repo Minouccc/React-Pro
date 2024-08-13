c3cc1abea29a58a1845bb99652e01a6fe0a424a5
Minouccc <343723899@qq.com>
2024-08-13 13:38:35 +0800

    feat: tailwind css

---
+++ tailwind-test/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-[14px] md:bg-blue-500 hover:text-[30px] p-1 border border-black border-solid">
+        guang
+      </div>
+      <div className="btn-primary">button</div>
+      <div className="btn-primary">button</div>
+      <div className="btn-primary">button</div>
+    </>
+  );
+}
+
+export default App;
